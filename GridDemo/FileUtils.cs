611bd36e6bbc36edaea8cf36499a425cceb49812
Lucas Teles <lucas.teles@lambda3.com.br>
2019-05-16 15:06:35 -0300

    adicao de polyfils para saveas

---
+++ GridDemo/FileUtils.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace GridDemo
+{
+  public static class FileUtil
+    {
+        public static Task SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
+}
